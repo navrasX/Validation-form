ac8eb5abb12861f84ad2c6f38bd8df723960306e
navrasX <80228487+navrasX@users.noreply.github.com>
2021-11-19 18:36:24 -0400

    Validation try for a single field
    First attempt a doing some validation for a single field.
    Moving forward to validate all of the fields in a better way.

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const nombre = document.getElementById(".namey");
-const apellido = document.getElementById(".surnamey");
-const email = document.getElementById(".emaily");
-const codey = document.getElementById(".codey");
+const nombre = document.getElementById("namey");
+const apellido = document.getElementById("surnamey");
+const email = document.getElementById("emaily");
+const codey = document.getElementById("codey");
 const loginy = document.querySelector(".login-btny");
+const messageError = document.querySelector(".messageError");
 
-if (nombre.value.length == 2 || apellido.value.length == 2 || email.value.indexOf("@") == -1) {
-    loginy.classList.remove('input[type="submit"]')
-    loginy.classList.add("red");
-} else {
-    loginy.classList.remove("red");
-    loginy.classList.add("green");
+
+loginy.addEventListener("click", (e) => {
+    if (codey.value.length < 6 || codey.value.length > 9) {
+        console.log("Doesn't work")
+        e.preventDefault()
+        codey.select()
+        messageError.innerHTML = "Inserte un código entre 6 y 9 caractéres"
+        messageError.classList.add("error")
+        loginy.classList.remove("greenBtn")
+        loginy.classList.add("redBtn")
+    } else {
+        console.log("It works")
+        messageError.innerHTML = " "
+        loginy.classList.remove("redBtn")
+        loginy.classList.add("greenBtn")
+        window.alert("Usuario validado. Inserte el siguiente")
+    }
+});
+
+
+if (email.focus() === true) {
+    loginy.classList.remove("redBtn")
 }
